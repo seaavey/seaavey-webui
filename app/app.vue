af7866b03ccc9abfb802804c1a698bfb8ca54d94
seaavey <seaavey@users.noreply.github.com>
2026-05-19 11:44:42 +0700

    chore: initial commit - web-ui

---
+++ app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1 class="text-3xl font-bold text-primary">
+    Hello world!
+  </h1>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@nuxtjs/color-mode'
+  ]
+})
